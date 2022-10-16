e6c3998bb232ae464244f62b65f51c12c7f13fe0
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-10-16 15:20:12 +0300

    Add files via upload

--- MyResource.lua
+++ MyResource.lua
@@ -1,8 +1,5 @@
-local lastHealt
-local effectBaseTime = 1000
-local damageMulti
 
-function text(content, position, color)
+function text(content, x, y, r, g, b)
     SetTextFont(7);
     SetTextScale(0.7f, 0.7f);
     SetTextColour(color[0], color[1], color[2], 255);
@@ -14,14 +11,14 @@ function text(content, position, color)
     DrawText(position.X, position.Y);
 end
 
-CreateThread(function()
+Citizen.CreateThread(function()
     while true do
         local player = GetPlayerPed(-1)
-        if player.IsPedInVehicle then
+        if IsPedInAnyVehicle(player) then
             local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1),false)
             local speed = GetEntitySpeed(vehicle)
-            local position = (0.9,0.95)
-            local color = (200,200,200)
-            text(math.floor(speed) + "KM/H",position,color)
+            local position = {x=0.9,y=0.95}
+            local color = {r=200,g=200,b=200}
+            text(math.floor(speed) + "KM/H",position.x,position.y,color.r,color.g,color.b)
         end
     end)
