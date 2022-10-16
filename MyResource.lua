d9248b55652cc546edbfb3d3b37b2b81b094390c
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-10-16 15:26:50 +0300

    Add files via upload

--- MyResource.lua
+++ MyResource.lua
@@ -1,20 +1,23 @@
 
 function text(content, x, y, r, g, b)
     SetTextFont(7);
+    SetTextProportional(0)
     SetTextScale(0.7f, 0.7f);
-    SetTextColour(color[0], color[1], color[2], 255);
+    SetTextColour(r, g, b, 255);
     SetTextEntry("STRING");
     SetTextDropshadow(0, 0, 0, 0, 255);
     SetTextDropShadow();
     SetTextOutline();
     AddTextComponentString(content);
-    DrawText(position.X, position.Y);
+    DrawText(x, y);
 end
 
 Citizen.CreateThread(function()
+
     while true do
+        Citizen.Wait(2)
         local player = GetPlayerPed(-1)
-        if IsPedInAnyVehicle(player) then
+        if (IsPedInAnyVehicle(player,false)) then
             local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1),false)
             local speed = GetEntitySpeed(vehicle)
             local position = {x=0.9,y=0.95}
