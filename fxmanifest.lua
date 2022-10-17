313cea38e12c7a0ad14b8c6ac523a9eeca8324a7
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-10-17 16:36:18 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,9 @@ author 'Vamma'
 description 'Realistic(ish) cars & accidents'
 version '1.0.0'
 
-client_scripts {
-    'MyResource.net.dll',
-    'MyResource.lua'
+files {
+
 }
+
+client_script 'MyResource.net.dll'
+server_script 'server.net.dll'
