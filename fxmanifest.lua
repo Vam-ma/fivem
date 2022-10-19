d1d574ebb118db91035929384358ed552047ae49
Vamma <58887108+Vam-ma@users.noreply.github.com>
2022-10-19 17:02:04 +0300

    Add files via upload

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,10 @@ game 'gta5'
 
 author 'Vamma'
 description 'Realistic(ish) cars & accidents'
-version '1.0.0'
+version '1.0.1'
 
 files {
-
+    'data/save.json'
 }
 
 client_script 'MyResource.net.dll'
